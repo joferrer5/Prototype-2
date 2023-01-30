384b80c3f1f7a9debbdada30127fecbf2aba2c8b
joferrer5 <joferrer@s.sfusd.edu>
2023-01-30 14:44:22 -0800

    Unit 2.2 Part 2

---
+++ Assets/Scripts/MoveFoward.cs
@@ -0,0 +1,20 @@
+ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveFoward : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public float speed = 40.0f;
+    public GameObject projectilePrefab;
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+}
