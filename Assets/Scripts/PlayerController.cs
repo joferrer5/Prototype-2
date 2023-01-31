315b2bf8ec1f7f5d8926888e3c7267522a464bec
joferrer5 <joferrer@s.sfusd.edu>
2023-01-31 14:29:47 -0800

    Unit 2 2.2 part 4

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
    public float speed = 10.0f;
    public float xRange = 10;
+   public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,6 +25,10 @@ public class PlayerController : MonoBehaviour
         if (transform.position.x < -xRange) {transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);}
         if (transform.position.x > xRange)
         {transform.position = new Vector3(xRange, transform.position.y, transform.position.z);}
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Launch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
     }
 }
