581458b825df0d8ff63fab18387e76f51fca26d8
joferrer5 <121971176+joferrer5@users.noreply.github.com>
2023-02-04 14:51:10 -0800

    Unit 2.3 Part 4

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -13,12 +13,16 @@ public class SpawnManager : MonoBehaviour
     }
 
     // Update is called once per frame
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.S)) 
         {
+           // Randomly generate animal index and spawn position
+           Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
             int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20), animalPrefabs[animalIndex].transform.rotation);
+            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
         }
     }
 }
